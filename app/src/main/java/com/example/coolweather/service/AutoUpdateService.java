9530e3d46995f90cebbf487204189b3db71e0ff2
butter007 <butter_007@163.com>
2018-05-10 22:16:23 +0800

    autoupdate 完成架子

---
+++ app/src/main/java/com/example/coolweather/service/AutoUpdateService.java
@@ -0,0 +1,32 @@
+package com.example.coolweather.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.SystemClock;
+
+public class AutoUpdateService extends Service {
+    public int onStartCommand(Intent intent,int flags,int startId){
+        updateWeather();
+        updanteBingPic();
+        AlarmManager manager = (AlarmManager)getSystemService(ALARM_SERVICE);
+        int anHour = 8*60*60*1000;
+        long triggerAtTime = SystemClock.elapsedRealtime()+anHour;
+        Intent i = new Intent(this,AutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this,0,i,0);
+        manager.cancel(pi);
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,triggerAtTime,pi);
+        return super.onStartCommand(intent,flags,startId);
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null
+                ;
+    }
+    public void updateWeather(){}
+
+    public void updanteBingPic(){}
+}
